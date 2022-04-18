50e844cba02139cde28b63f39c01867a380d7ef0
dsctt <dascott321@gmail.com>
2022-04-17 23:15:03 -0400

    nchain

--- gym/envs/toy_text/nchain.py
+++ gym/envs/toy_text/nchain.py
@@ -23,7 +23,7 @@ class NChainEnv(gym.Env):
     http://ceit.aut.ac.ir/~shiry/lecture/machine-learning/papers/BRL-2000.pdf
     """
 
-    def __init__(self, n=5, slip=0.2, small=2, large=10):
+    def __init__(self, n=5, slip=0.2, small=2, large=10, max_iters=1000):
         self.n = n
         self.slip = slip  # probability of 'slipping' an action
         self.small = small  # payout for 'backwards' action
@@ -31,6 +31,41 @@ class NChainEnv(gym.Env):
         self.state = 0  # Start at beginning of the chain
         self.action_space = spaces.Discrete(2)
         self.observation_space = spaces.Discrete(self.n)
+        self.iters = 0
+        self.max_iters = max_iters
+
+        # Setup transitions and rewards
+        self.P = {s: {a: [] for a in range(2)} for s in range(n)}
+        for state in self.P.keys():
+            d = {}
+            for action in self.P[state].keys():
+                res = []
+
+                f_reward = 0
+                next_state = state + 1
+
+                if state == self.n-1:
+                    f_reward = large
+                    next_state = state
+
+                if action == 0: # forward is chosen
+
+                    t1 = (1-slip, next_state, f_reward, None)
+                    t2 = (slip, 0, small, None)
+                    res.append(t1)
+                    res.append(t2)
+
+                else: # backward is chosen
+
+                    t1 = (1-slip, 0, small, None)
+                    t2 = (slip, next_state, f_reward, None)
+                    res.append(t1)
+                    res.append(t2)
+
+                d[action] = res
+            
+            self.P[state] = d
+
         self.seed()
 
     def seed(self, seed=None):
@@ -49,7 +84,13 @@ class NChainEnv(gym.Env):
             self.state += 1
         else:  # 'forwards': stay at the end of the chain, collect large reward
             reward = self.large
+
         done = False
+        self.iters += 1
+        if self.iters == self.max_iters:
+            done = True
+            self.iters = 0
+
         return self.state, reward, done, {}
 
     def reset(self):
